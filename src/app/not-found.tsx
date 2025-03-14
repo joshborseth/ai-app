3e6e824bc07304e42503e0333db12d1e4e0e62c0
joshborseth <joshuaborseth@gmail.com>
2025-03-13 18:47:31 -0700

    idk man

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,11 +3,11 @@ import { buttonVariants } from "~/components/ui/button";
 
 export default function NotFound() {
   return (
-    <main className="bg-bg flex min-h-[100dvh] w-full items-center justify-center bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
+    <main className="flex min-h-[100dvh] w-full items-center justify-center bg-bg bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
       <div className="flex flex-col gap-8 p-4">
         <div className="flex max-w-sm flex-col items-center gap-4 text-center">
           <h1 className="text-5xl font-black">404 - Not Found</h1>
-          <p className="font-base text-xl">
+          <p className="text-xl font-base">
             Oops! Looks like you&apos;re lost.
           </p>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,10 @@ import { buttonVariants } from "~/components/ui/button";
 export default function Home() {
   return (
     <Background className="flex items-center justify-center">
-      <div className="animate-fade-in flex flex-col gap-8 p-4">
+      <div className="flex animate-fade-in flex-col gap-8 p-4">
         <div className="flex max-w-sm flex-col items-center gap-4 text-center">
-          <h1 className="text-5xl font-black">IDK what the name is yet.</h1>
-          <p className="font-base text-xl">
-            An AI powered tool to help you in your darkest hour.
-          </p>
+          <h1 className="text-6xl font-black lg:text-7xl">Scottie</h1>
+          <h2 className="text-2xl font-base">An AI powered email client.</h2>
         </div>
         <Link
           href="/get-started"
